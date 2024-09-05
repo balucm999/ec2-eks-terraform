534e1b9963b20de20ff54ad92af19eafecb9a9cf
balucm999 <balamuralikrishna@emmsonsinfotech.com>
2024-09-05 21:00:08 +0530

    Update backend.tf

--- EKS_ArgoCD_AWS_LB_Controller_Terraform/backend.tf
+++ EKS_ArgoCD_AWS_LB_Controller_Terraform/backend.tf
@@ -5,11 +5,11 @@ terraform {
     region = "us-east-1"
   }
 }
-terraform {
-  required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.10.0"
-    }
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.eks_cluster.endpoint
+    token                  = data.aws_eks_cluster_auth.eks_cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
   }
 }
+
