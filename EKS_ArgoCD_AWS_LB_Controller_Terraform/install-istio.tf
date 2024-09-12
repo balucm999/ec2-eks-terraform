4e691fb2fd7e289008f4bd24ab7ea1ada27631fa
balucm999 <balamuralikrishna@emmsonsinfotech.com>
2024-09-12 18:45:14 +0530

    Update install-istio.tf

--- EKS_ArgoCD_AWS_LB_Controller_Terraform/install-istio.tf
+++ EKS_ArgoCD_AWS_LB_Controller_Terraform/install-istio.tf
@@ -1,17 +1,29 @@
+# Ensure kubeconfig is updated
+resource "null_resource" "update_kubeconfig" {
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --region us-west-1 --name k8s-cluster"
+  }
+
+  depends_on = [
+    module.eks
+  ]
+}
+
 # Ensure install-istio.sh is executable
 resource "null_resource" "ensure_istio_script_executable" {
   provisioner "local-exec" {
     command = "chmod +x ./install-istio.sh"
   }
 }
-# Run install-istio.sh after EKS cluster is provisioned
+
+# Run install-istio.sh after kubeconfig is updated
 resource "null_resource" "run_istio_script" {
   provisioner "local-exec" {
     command = "./install-istio.sh"
   }
 
   depends_on = [
-    module.eks,
+    null_resource.update_kubeconfig,
     null_resource.ensure_istio_script_executable
   ]
 }
